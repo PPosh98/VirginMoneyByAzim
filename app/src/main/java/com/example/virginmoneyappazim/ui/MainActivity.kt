dbccd0c83687f94b8ec84b71c123e2d4be42e5a5
Azim Shah <azim.1998@hotmail.com>
2022-08-06 09:29:13 +0100

    create project with dependencies

---
+++ app/src/main/java/com/example/virginmoneyappazim/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.virginmoneyappazim.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.virginmoneyappazim.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
