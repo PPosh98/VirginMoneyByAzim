632f12f1979d3d51e4ed0b690332b526c3897553
Azim Shah <azim.1998@hotmail.com>
2022-08-07 10:33:55 +0100

    add NetworkModule class  for Dependency Injection

---
+++ app/src/main/java/com/example/virginmoneyappazim/di/NetworkModule.kt
@@ -0,0 +1,44 @@
+package com.example.virginmoneyappazim.di
+
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    fun provideGson() = Gson()
+
+    @Provides
+    fun httpLoggingInterceptor() =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Provides
+    fun okHttpClient(loggingInterceptor: HttpLoggingInterceptor) =
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+    @Provides
+    fun retrofitBuilder(gson: Gson, okHttpClient: OkHttpClient) =
+        Retrofit.Builder()
+            .baseUrl("https://61e947967bc0550017bc61bf.mockapi.io/api/v1/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClient)
+            .build()
+
+}
